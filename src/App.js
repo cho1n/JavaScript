61193fdc012aea665a8459d2db643ae7543baf05
cho1n <cjaewon0217@naver.com>
2023-03-18 20:06:18 +0900

    Second

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={ {color : 'yellow', fontSize : '16px'} }>블로그</h4>
+      </div>
+      <h4>{ post }</h4>
     </div>
   );
 }
 
 export default App;
+
+// class 넣을 땐 className
+// 변수 넣을 땐 {변수명} = 데이터바인딩
+// style 넣을 땐 style = {{이름:'값'}}
